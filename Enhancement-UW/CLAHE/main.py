a8b1cda3ea7935ca30a897f2cbddb8f2dbd80577
karthik-d <karthikdesingu2000@gmail.com>
2022-03-20 21:15:14 +0530

    dir struct added

--- Enhancement-UW/CLAHE/main.py
+++ Enhancement-UW/CLAHE/main.py
@@ -8,25 +8,26 @@ from skimage import exposure
 from sceneRadianceCLAHE import RecoverCLAHE
 from sceneRadianceHE import RecoverHE
 
-np.seterr(over='ignore')
-if __name__ == '__main__':
-    pass
-# folder = "C:/Users/Administrator/Desktop/UnderwaterImageEnhancement/HE"
-folder = "C:/Users/Administrator/Desktop/UnderwaterImageEnhancement/NonPhysical/ClAHE"
-# folder = "C:/Users/Administrator/Desktop/Databases/Dataset"
+from ..config import config
 
-path = folder + "/InputImages"
-files = os.listdir(path)
-files =  natsort.natsorted(files)
+def run(base_path=None, input_dirname=None):
 
-for i in range(len(files)):
-    file = files[i]
-    filepath = path + "/" + file
-    prefix = file.split('.')[0]
-    if os.path.isfile(filepath):
-        print('********    file   ********',file)
-        # img = cv2.imread('InputImages/' + file)
-        img = cv2.imread(folder + '/InputImages/' + file)
-        sceneRadiance = RecoverCLAHE(img)
-        # cv2.imwrite('OutputImages/' + prefix + '_CLAHE.jpg', sceneRadiance)
-        cv2.imwrite('Temps/' + prefix + '_CLAHE.jpg', sceneRadiance)
+	if base_path is None:
+		base_path = config.get('BASE_PATH')
+	if input_dirname is None:
+		input_dirname = config.get('INPUT_DIRNAME')
+
+	path = os.path.join(base_path, input_dirname)
+	files = os.listdir(path)
+	files =  natsort.natsorted(files)
+	for i in range(len(files)):
+		file = files[i]
+		filepath = path + "/" + file
+		prefix = file.split('.')[0]
+		if os.path.isfile(filepath):
+			print('********    file   ********',file)
+			# img = cv2.imread('InputImages/' + file)
+			img = cv2.imread(folder + '/InputImages/' + file)
+			sceneRadiance = RecoverCLAHE(img)
+			# cv2.imwrite('OutputImages/' + prefix + '_CLAHE.jpg', sceneRadiance)
+			cv2.imwrite('Temps/' + prefix + '_CLAHE.jpg', sceneRadiance)
---
+++ Enhancement-UW/config.py
@@ -0,0 +1,7 @@
+import os
+
+config = dict(
+    BASE_PATH = os.path.realpath(__file__)
+    INPUT_DIRNAME = 'inputs',
+    OUPUT_DIRNAME = 'outputs'
+}
